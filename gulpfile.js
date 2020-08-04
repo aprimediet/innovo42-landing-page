01bbc296049e5a45b40aa6b8b1bfa378418bf982
Aditya Prima <aprimediet@gmail.com>
2020-08-05 01:14:44 +0700

    rework scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 const devBuild = (process.env.NODE_ENV !== 'production'),
       fs = require('fs'),
       src = './src',
+      xsrc = './xsrc',
       dist = './dist',
+      xdist = './xdist',
       gulp = require('gulp'),
       data = require('gulp-data'),
       noop = require('gulp-noop'),
@@ -28,12 +30,28 @@ function clean () {
   ])
 }
 
+function xClean () {
+  return del([
+    `${xdist}/**/*.html`,
+    `${xdist}/**/*.css`,
+    `${xdist}/images/*`
+  ])
+}
+
 function images () {
   return gulp
     .src(`${src}/images/**/*`)
     .pipe(newer(`${dist}/images/`))
     .pipe(imagemin({ optimizationLevel: 5 }))
     .pipe(gulp.dest(`${dist}/images/`))
+}
+
+function xImages () {
+  return gulp
+    .src(`${src}/images/**/*`)
+    .pipe(newer(`${xdist}/images/`))
+    .pipe(imagemin({ optimizationLevel: 5 }))
+    .pipe(gulp.dest(`${xdist}/images/`))
 }
 
 function html () {
@@ -46,6 +64,18 @@ function html () {
     .pipe(pug())
     .pipe(devBuild ? prettyHtml() : htmlClean())
     .pipe(gulp.dest(dist))
+}
+
+function xHtml () {
+  return gulp
+    .src(`${xsrc}/pages/**/*.pug`)
+    .pipe(newer(xdist))
+    .pipe(data(() => {
+      return JSON.parse(fs.readFileSync(`${src}/data/main.json`))
+    }))
+    .pipe(pug())
+    .pipe(devBuild ? prettyHtml() : htmlClean())
+    .pipe(gulp.dest(xdist))
 }
 
 function sharedHtml () {
@@ -81,10 +111,39 @@ function css () {
     .pipe(gulp.dest(`${dist}`))
 }
 
+function xCss () {
+  return gulp
+    .src(`${xsrc}/scss/main.scss`)
+    .pipe(sourcemaps ? sourcemaps.init() : noop())
+    .pipe(sass({
+      outputStyle: 'nested',
+      imagePath: '/images/',
+      precision: 3,
+      errLogToConsole: true
+    }))
+    .on('error', sass.logError)
+    .pipe(postcss([
+      assets({ loadPaths: ['images/']}),
+      autoprefixer({ overrideBrowserslist: ['last 2 versions', '> 2%']}),
+      mqpacker,
+      cssnano
+    ]))
+    .pipe(sourcemaps ? sourcemaps.write() : noop())
+    .pipe(gulp.dest(`${xdist}`))
+}
+
 function watch (done) {
   gulp.watch(`${src}/data/**/*.json`, gulp.parallel(html, sharedHtml))
   gulp.watch('**/*.pug', gulp.parallel(html, sharedHtml))
   gulp.watch('**/*.scss', css)
+
+  done()
+}
+
+function xWatch (done) {
+  gulp.watch(`${src}/data/**/*.json`, xHtml)
+  gulp.watch(`${xsrc}/**/*.pug`, xHtml)
+  gulp.watch(`${xsrc}/**/*.scss`, xCss)
 
   done()
 }
@@ -99,9 +158,26 @@ function runBrowser () {
   gulp.watch(`${dist}/**/*`).on('change', browserSync.reload)
 }
 
+function xRunBrowser () {
+  browserSync.init({
+    server: {
+      baseDir: xdist,
+    },
+  })
+
+  gulp.watch(`${xdist}/**/*`).on('change', browserSync.reload)
+}
+
 exports.images = images
 exports.html = gulp.series(images, html, sharedHtml)
 exports.css = gulp.series(images, css)
 exports.build = gulp.series(clean, exports.html, exports.css)
 exports.watch = watch
-exports.default = gulp.series(exports.build, exports.watch, runBrowser)
+
+exports.xImages = xImages
+exports.xHtml = gulp.series(xImages, xHtml)
+exports.xCss = gulp.series(xImages, xCss)
+exports.xBuild = gulp.series(xClean, exports.xHtml, exports.xCss)
+exports.xWatch = xWatch
+
+exports.default = gulp.series(exports.xBuild, exports.xWatch, xRunBrowser)
